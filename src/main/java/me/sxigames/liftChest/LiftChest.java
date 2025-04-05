c1dcdb602154008b928552c47f455f9548068a29
Jorge Felipe <sxigames63@gmail.com>
2025-04-05 15:36:57 -0300

    Initial Commit

---
+++ src/main/java/me/sxigames/liftChest/LiftChest.java
@@ -0,0 +1,17 @@
+package me.sxigames.liftChest;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class LiftChest extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
