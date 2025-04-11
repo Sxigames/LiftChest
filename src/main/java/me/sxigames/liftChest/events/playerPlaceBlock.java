47b37a811dcc6409d3acef59fb8fc25be412d7bf
Jorge Felipe <sxigames63@gmail.com>
2025-04-10 22:25:01 -0300

    Refactor chest lifting logic to support various container types and improve item handling

--- src/main/java/me/sxigames/liftChest/events/playerPlaceBlock.java
+++ src/main/java/me/sxigames/liftChest/events/playerPlaceBlock.java
@@ -6,8 +6,8 @@ import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 import org.bukkit.attribute.Attribute;
 import org.bukkit.block.Block;
-import org.bukkit.block.Chest;
-import org.bukkit.entity.ItemDisplay;
+import org.bukkit.block.Container;
+import org.bukkit.block.Furnace;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -24,28 +24,30 @@ public class playerPlaceBlock implements Listener {
     public void onBlockPlace(BlockPlaceEvent event) {
         Block block = event.getBlock();
         Player player = event.getPlayer();
-        if(block.getType() == Material.STONE_BUTTON && player.getScoreboardTags().contains("carrying")){
+        if(block.getState() instanceof Container container && player.getScoreboardTags().contains("carrying")){
             player.getPassengers().forEach((passenger) -> {
-                if (passenger.getScoreboardTags().contains("carried") && passenger instanceof ItemDisplay itemDisplay) {
+                if (passenger.getScoreboardTags().contains("carried")) {
                     Plugin plugin = LiftChest.getPlugin();
-                    block.setType(itemDisplay.getItemStack().getType());
-                    org.bukkit.block.data.type.Chest chestData = (org.bukkit.block.data.type.Chest) block.getBlockData();
-                    chestData.setType(org.bukkit.block.data.type.Chest.Type.SINGLE);
-                    chestData.setFacing(player.getFacing().getOppositeFace());
-                    block.setBlockData(chestData);
-                    NamespacedKey chestKey = new NamespacedKey(plugin, "chestData");
-                    ItemStack[] items = ItemStack.deserializeItemsFromBytes(Objects.requireNonNull(passenger.getPersistentDataContainer().get(chestKey, PersistentDataType.BYTE_ARRAY)));
+                    NamespacedKey inventoryKey = new NamespacedKey(plugin, "inventoryBytes");
+                    ItemStack[] items = ItemStack.deserializeItemsFromBytes(Objects.requireNonNull(passenger.getPersistentDataContainer().get(inventoryKey, PersistentDataType.BYTE_ARRAY)));
                     PlayerInventory inventory = player.getInventory();
                     inventory.setItemInMainHand(ItemStack.of(Material.AIR));
                     inventory.setItemInOffHand(ItemStack.of(Material.AIR));
-                    Chest chestState = (Chest) block.getState();
-                    NamespacedKey chestNameKey = new NamespacedKey(plugin, "chestName");
-                    if (passenger.getPersistentDataContainer().has(chestNameKey, PersistentDataType.STRING)){
-                        String chestName = Objects.requireNonNull(passenger.getPersistentDataContainer().get(chestNameKey, PersistentDataType.STRING));
-                        chestState.customName(MiniMessage.miniMessage().deserialize(chestName));
-                        chestState.update();
+                    NamespacedKey containerNameKey = new NamespacedKey(plugin, "containerName");
+                    if (passenger.getPersistentDataContainer().has(containerNameKey, PersistentDataType.STRING)){
+                        String containerName = Objects.requireNonNull(passenger.getPersistentDataContainer().get(containerNameKey, PersistentDataType.STRING));
+                        container.customName(MiniMessage.miniMessage().deserialize(containerName));
                     }
-                    chestState.getBlockInventory().setStorageContents(items);
+                    else {
+                        container.customName(null);
+                    }
+                    if (container instanceof Furnace furnace){
+                        NamespacedKey burnTimeKey = new NamespacedKey(plugin, "burnTime");
+                        short burnTime = Objects.requireNonNull(passenger.getPersistentDataContainer().get(burnTimeKey, PersistentDataType.SHORT));
+                        furnace.setBurnTime(burnTime);
+                    }
+                    container.update();
+                    container.getInventory().setStorageContents(items);
                     passenger.remove();
                     player.removeScoreboardTag("carrying");
                     NamespacedKey slowKey = new NamespacedKey(plugin, "carryingSlow");
